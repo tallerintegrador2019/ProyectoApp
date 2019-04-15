d8b1993570141b8766a88086fea897e8a5797ae1
rober fernandez <rcfernandez@outlook.com.ar>
2019-04-15 19:08:47 -0300

    agregado funcionalidad camara

--- src/app/publicar/publicar.module.ts
+++ src/app/publicar/publicar.module.ts
@@ -7,6 +7,8 @@ import { IonicModule } from '@ionic/angular';
 
 import { PublicarPage } from './publicar.page';
 
+import { Camera,CameraOptions } from "@ionic-native/camera/ngx"; // 01.importamos la camara
+
 const routes: Routes = [
   {
     path: '',
@@ -23,4 +25,30 @@ const routes: Routes = [
   ],
   declarations: [PublicarPage]
 })
-export class PublicarPageModule {}
+export class PublicarPageModule {
+
+  image: string;
+
+  constructor(private camera : Camera) {
+  }
+
+  public takePicture() {
+    
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     console.log (err);
+    });
+
+  }
+
+}
