8114312de5471e03b0634e00b6d79250dcdfc467
rober <rcfernandez@outlook.com.ar>
2019-04-14 22:01:39 -0300

    agregado texto publicar al menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
+  },
+  { 
+    path: 'publicar', 
+    loadChildren: './publicar/publicar.module#PublicarPageModule' 
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,11 @@ export class AppComponent {
       title: 'List',
       url: '/list',
       icon: 'list'
+    },
+    {
+      title: 'Publicar Articulo',
+      url: '/publicar',
+      icon: 'add-circle-outline'
     }
   ];
 
