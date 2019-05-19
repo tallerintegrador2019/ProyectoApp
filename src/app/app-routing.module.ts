135866e943d06f329657168791cc04fa1e7062cf
rober <rcfernandez@outlook.com.ar>
2019-05-19 19:23:45 -0300

    se agrego tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,24 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  { path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
+  { path: 'publicar', loadChildren: './pages/publicar/publicar.module#PublicarPageModule' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: 'publicacion/:id', loadChildren: './pages/publicacion/publicacion.module#PublicacionPageModule' },
+  { path: 'publicacion', loadChildren: './pages/publicacion/publicacion.module#PublicacionPageModule' }
+
+
+  /* { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
   { path: 'list', loadChildren: './components/list/list.module#ListPageModule' },
   { path: 'publicar', loadChildren: './pages/publicar/publicar.module#PublicarPageModule' },
   { path: 'publicacion/:id', loadChildren: './pages/publicacion/publicacion.module#PublicacionPageModule' },
-  { path: 'publicacion', loadChildren: './pages/publicacion/publicacion.module#PublicacionPageModule' }
+  { path: 'publicacion', loadChildren: './pages/publicacion/publicacion.module#PublicacionPageModule' },
+  { path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
+  { path: 'tab1', loadChildren: './pages/tab1/tab1.module#Tab1PageModule' },
+  { path: 'tab2', loadChildren: './pages/tab2/tab2.module#Tab2PageModule' } */
+
   
 
 ];
